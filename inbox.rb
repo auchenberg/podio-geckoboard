0fc51c08936face47a27a1e73b93586036a83553
Kenneth Auchenberg <kenneth@auchenberg.dk>
2011-08-19 15:31:18 +0200

    test

--- inbox.rb
+++ inbox.rb
@@ -4,7 +4,7 @@ require 'podio'
 
 get '/' do
   Podio.client = Podio::Client.new({:api_key => 'inboxcounter', :api_secret => 'DpveglhC8ZzxiqPjOXGJd150PRBGAv5UEKeoLcdMUp9oocNOxc8VGTHmrVnkVAh6'})
-  Podio.client.authenticate_with_credentials(params['user'], params['user'])
+  Podio.client.authenticate_with_credentials(params['u'], params['p'])
   
   user = Podio::UserStatus.current
   inbox_count = user.inbox_new
