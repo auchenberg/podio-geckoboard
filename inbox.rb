5823dfabe0e1c0e9259b7c209a0cbfe89d81884c
Kenneth Auchenberg <kenneth@auchenberg.dk>
2011-09-23 13:04:03 +0200

    more fun

--- inbox.rb
+++ inbox.rb
@@ -1,14 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 require 'podio'
+require 'json'
 
 get '/' do
+  content_type :json
   Podio.client = Podio::Client.new({:api_key => 'inboxcounter', :api_secret => 'DpveglhC8ZzxiqPjOXGJd150PRBGAv5UEKeoLcdMUp9oocNOxc8VGTHmrVnkVAh6'})
   Podio.client.authenticate_with_credentials(params['u'], params['p'])
-  
-  user = Podio::UserStatus.current
-  inbox_count = user.inbox_new
-  
-  "#{user.inbox_new}"
+
+
+  user = Podio::UserStatus.current  
+  puts "#{user.inbox_new}"
+
+  { :item => user.inbox_new, :max => { :text => 'max inbox', :value => "100" }, :min => { :text => 'min inbox', :value => "0" } }.to_json
+
+
+
+
 end
 
